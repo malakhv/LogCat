3db8256ee39b8f4dbb2d5e77630f6c03f07b5c7e
Mikhail Malakhov <malakhv@live.ru>
2016-06-15 20:32:42 +0300

    Added test engine to TestApp module

--- TestApp/src/main/java/com/malakhv/libs/logcat/test/MainActivity.java
+++ TestApp/src/main/java/com/malakhv/libs/logcat/test/MainActivity.java
@@ -2,12 +2,57 @@ package com.malakhv.libs.logcat.test;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends Activity {
+import com.malakhv.util.LogCat;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+    private final static String TAG = "LogTest";
+
+    /**
+     * setprop log.tag.xLogLib ERROR
+     * setprop log.tag.xLogLib INFO
+     * setprop log.tag.xLogLib DEBUG
+     * setprop log.tag.xLogLib VERBOSE
+     * */
+    static {
+        LogCat.init("xLogLib");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Called when a view has been clicked.
+     *
+     * @param v The view that was clicked.
+     */
+    @Override
+    public void onClick(View v) {
+        final int id = v != null ? v.getId() : 0;
+
+        if (id == R.id.btn_e) {
+            LogCat.e(TAG, "Log level - ERROR");
+        }
+
+        if (id == R.id.btn_w) {
+            LogCat.w(TAG, "Log level - WARN");
+        }
+
+        if (id == R.id.btn_i) {
+            LogCat.w(TAG, "Log level - INFO");
+        }
+
+        if (id == R.id.btn_d) {
+            LogCat.d(TAG, "Log level - DEBUG");
+        }
+
+        if (id == R.id.btn_v) {
+            LogCat.v(TAG, "Log level - VERBOSE");
+        }
+
     }
 }
